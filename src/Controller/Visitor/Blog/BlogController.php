a4d34ae8ff6877bcd63772fb090f48c3b9d0bc7b
gio <sergiozezo@gmail.com>
2025-04-24 10:20:06 +0200

    TEST

---
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller\Visitor\Blog;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController{
+    #[Route('/blog', name: 'app_visitor_blog')]
+    public function index(): Response
+    {
+        return $this->render('pages/visitor/blog/index.html.twig');
+    }
+}
